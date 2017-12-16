7bf6903386d588d83d9ff79a246fadbeaf00833a
Benny Lach <benny@kptncook.com>
2017-12-16 11:38:24 +0100

    Project init

---
+++ SharedSources/BLMigration.h
@@ -0,0 +1,19 @@
+//
+//  BLMigration_iOS.h
+//  BLMigration_iOS
+//
+//  Created by Benny Lach on 16.12.17.
+//  Copyright © 2017 Benny Lach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BLMigration_iOS.
+FOUNDATION_EXPORT double BLMigration_iOSVersionNumber;
+
+//! Project version string for BLMigration_iOS.
+FOUNDATION_EXPORT const unsigned char BLMigration_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLMigration_iOS/PublicHeader.h>
+
+
